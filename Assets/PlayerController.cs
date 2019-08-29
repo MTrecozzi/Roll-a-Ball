bc48ad964065cdaeeea2b6c5c8957eb56a94e2cf
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-08-29 12:29:19 -0400

    Added Player Movement Script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    public float moveH;
+    public float moveV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0f, moveV);
+
+        rb.AddForce(movement * speed);
+        
+    }
+}
