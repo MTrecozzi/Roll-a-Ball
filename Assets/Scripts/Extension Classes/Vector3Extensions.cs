0c1b79bfd118600ac29c81a53bf54981e6050e42
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-08-31 00:33:31 -0400

    Implemented Touch Controls

---
+++ Assets/Scripts/Extension Classes/Vector3Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3Extensions
+{
+    public static Vector3 With(this Vector3 original, float? x = null, float ? y = null, float ? z = null){
+
+        // Was X set? if not use original value, if so use the set value in the constructor
+        return new Vector3(x ?? original.x, y ?? original.y, z?? original.z);
+    }
+
+    public static Vector3 Flat (this Vector3 original){
+        return new Vector3 (original.x, 0, original.z);
+    }
+
+    public static Vector3 DirectionTo (this Vector3 source, Vector3 destination){
+        return (destination - source).normalized;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,17 +11,60 @@ public class PlayerController : MonoBehaviour
     public float moveH;
     public float moveV;
 
+    public Vector3 touchDirection;
+
+    public bool touchOverride = true;
+
     // Start is called before the first frame update
     void Start()
     {
         this.rb = GetComponent<Rigidbody>();
     }
 
+    public void Update(){
+
+        if (Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            RaycastHit hit;
+
+          if (Physics.Raycast(ray, out hit)){
+              Debug.Log(hit.point);
+            }
+
+        var curPosition = transform.position.With(y: 0);
+
+        var hitPos = hit.point.With(y: 0);
+
+        var touchDir = (hitPos - curPosition);
+
+        touchDir = Vector3.Normalize(touchDir);
+
+        touchDirection = touchDir;
+
+        Debug.Log(touchDirection);
+
+        } else if (Input.touchCount == 0){
+            touchDirection = Vector3.zero;
+        }
+
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        moveH = Input.GetAxis("Horizontal");
-        moveV = Input.GetAxis("Vertical");
+        if (!touchOverride){
+            moveH = Input.GetAxis("Horizontal");
+            moveV = Input.GetAxis("Vertical");
+        } else {
+            moveH = touchDirection.x;
+            moveV = touchDirection.z;
+        }
+
+
+        
 
         Vector3 movement = new Vector3(moveH, 0f, moveV);
 
