d892f82530554868f326586c27be3bcab0818e30
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-02 13:49:26 -0400

    Rudimentary Slope Detection
    When attempting to move up a slope, the ball will adjust its target velocity to better preserve momentum over ramps.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,13 @@ public class PlayerController : MonoBehaviour
 
     public Vector3 touchDirection;
 
-    public bool touchOverride = true;
+    public bool touchOverride = false;
+
+    public Collider col;
+
+    public LayerMask ground;
+
+    public Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +29,12 @@ public class PlayerController : MonoBehaviour
 
     public void Update(){
 
+        touchOverride = false;
+
         if (Input.touchCount > 0){
+
+            touchOverride = true;
+
             Touch touch = Input.GetTouch(0);
 
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
@@ -31,7 +42,7 @@ public class PlayerController : MonoBehaviour
             RaycastHit hit;
 
           if (Physics.Raycast(ray, out hit)){
-              Debug.Log(hit.point);
+              //Debug.Log(hit.point);
             }
 
         var curPosition = transform.position.With(y: 0);
@@ -44,7 +55,7 @@ public class PlayerController : MonoBehaviour
 
         touchDirection = touchDir;
 
-        Debug.Log(touchDirection);
+        //Debug.Log(touchDirection);
 
         } else if (Input.touchCount == 0){
             touchDirection = Vector3.zero;
@@ -55,6 +66,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        velocity = rb.velocity;
+
         if (!touchOverride){
             moveH = Input.GetAxis("Horizontal");
             moveV = Input.GetAxis("Vertical");
@@ -63,10 +77,37 @@ public class PlayerController : MonoBehaviour
             moveV = touchDirection.z;
         }
 
+        Vector3 moveVector = new Vector3(moveH, 0, moveV);
+        moveVector = moveVector.normalized;
+
+        RaycastHit hit2 = new RaycastHit();
+
+        Vector3 transformCastPoint = transform.position + new Vector3(0, -col.bounds.extents.y + 0.01f, 0);
+
+        Physics.Raycast(transformCastPoint, moveVector, out hit2, 1f, ground);
+
+
+        float slopeHeight = 0f;
+
+        if (hit2.transform != null && hit2.collider != null){
+            Debug.Log(hit2.normal);
+
+            float slopeNormalX = Mathf.Abs(hit2.normal.x);
+            float slopeNormalY = Mathf.Abs(hit2.normal.z);
+            
+            slopeHeight = slopeNormalX;
+
+            if (slopeNormalY > slopeNormalX){
+                slopeHeight = slopeNormalY;
+            }
+
+        }
+
+        Vector3 movement = new Vector3(moveH, slopeHeight, moveV);
 
         
 
-        Vector3 movement = new Vector3(moveH, 0f, moveV);
+        Debug.Log("Movement Vector3: " + movement);
 
         rb.AddForce(movement * speed);
         
